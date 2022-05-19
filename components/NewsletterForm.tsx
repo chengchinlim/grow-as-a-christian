18c025d7c4128fe780e91cad3de0cfccd2bbf119
Cheng Dev <chengdev@Chengs-MBP-3.lan>
2022-05-18 23:04:32 -0500

    Update NewsLetterForm text

--- components/NewsletterForm.tsx
+++ components/NewsletterForm.tsx
@@ -2,7 +2,7 @@ import React, { useRef, useState } from 'react'
 
 import siteMetadata from '@/data/siteMetadata'
 
-const NewsletterForm = ({ title = 'Subscribe to the newsletter' }) => {
+const NewsletterForm = ({ title = 'Subscribe to get notified when there is a new blog.' }) => {
   const inputEl = useRef<HTMLInputElement>(null)
   const [error, setError] = useState(false)
   const [message, setMessage] = useState('')
