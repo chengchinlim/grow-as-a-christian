18a79573091d88f566e9f75a362c56047c670a67
Cheng Chin Lim <chengchin5227@gmail.com>
2022-05-30 23:35:07 -0500

    Print whole error object

--- pages/api/mailchimp.ts
+++ pages/api/mailchimp.ts
@@ -21,7 +21,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     })
     return res.status(201).json({ error: '' })
   } catch (error) {
-    console.log(error.response.text)
+    console.log(error)
     return res.status(500).json({ error: error.message || error.toString() })
   }
 }
